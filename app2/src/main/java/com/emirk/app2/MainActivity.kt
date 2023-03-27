b84fa2e5e33d1290a3d7ec64e099b949dc35775f
emirk <emir.karabey1@hotmail.com>
2023-03-27 17:28:55 +0300

    Use data binding on main activity

--- app2/src/main/java/com/emirk/app2/MainActivity.kt
+++ app2/src/main/java/com/emirk/app2/MainActivity.kt
@@ -2,10 +2,21 @@ package com.emirk.app2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.emirk.app2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        onBtnClicked()
+    }
+
+    private fun onBtnClicked() = dataBinding.btnClick.setOnClickListener {
+        val person = Person("Emir", "Karabey", "22")
+        dataBinding.person = person
     }
 }
