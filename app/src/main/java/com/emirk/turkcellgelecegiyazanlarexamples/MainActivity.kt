95229eeff7e2931a55c26fb60d77a4f0ad93ec52
emirk <emir.karabey1@hotmail.com>
2023-03-27 16:53:34 +0300

    Use view binding on main activity

--- app/src/main/java/com/emirk/turkcellgelecegiyazanlarexamples/MainActivity.kt
+++ app/src/main/java/com/emirk/turkcellgelecegiyazanlarexamples/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.emirk.turkcellgelecegiyazanlarexamples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emirk.turkcellgelecegiyazanlarexamples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onBtnClick()
     }
+
+    private fun onBtnClick() = binding.btnCount.setOnClickListener {
+        count += 1
+        binding.tvCounter.text = count.toString()
+    }
+
 }
