93386a200035806d890e0420ba4a1a4df7ffb26f
emirk <emir.karabey1@hotmail.com>
2023-03-29 03:51:56 +0300

    Create room module and add hilt

---
+++ app4/src/main/java/com/emirk/app4/di/RoomModule.kt
@@ -0,0 +1,31 @@
+package com.emirk.app4.di
+
+import android.content.Context
+import androidx.room.Room
+import com.emirk.app4.data.local.FavoriteDao
+import com.emirk.app4.data.local.FavoriteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+const val DATABASE_NAME = "person"
+@[Module InstallIn(SingletonComponent::class)]
+object StoreRoomModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): FavoriteDatabase{
+        return Room.databaseBuilder(context, FavoriteDatabase::class.java, DATABASE_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDao(
+        db:FavoriteDatabase
+    ):FavoriteDao = db.favoriteDao()
+}
