18b0735b0540cee566496825721ca57e8b49e531
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-04 15:30:55 +0300

    Create models

---
+++ imdbexample/src/main/java/com/emirk/imdbexample/data/remote/model/Genre.kt
@@ -0,0 +1,10 @@
+package com.emirk.imdbexample.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
---
+++ imdbexample/src/main/java/com/emirk/imdbexample/data/remote/model/SpokenLanguage.kt
@@ -0,0 +1,13 @@
+package com.emirk.imdbexample.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class SpokenLanguage(
+    @SerializedName("english_name")
+    val english_name: String,
+    @SerializedName("iso_639_1")
+    val iso_639_1: String,
+    @SerializedName("name")
+    val name: String
+)
